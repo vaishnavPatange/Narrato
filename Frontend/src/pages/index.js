8183badbd4e8116367a7a4695df010e67b12fc5d
vaishnavPatange <patangevaishnav5@gmail.com>
2024-09-01 19:15:43 +0530

    React done

---
+++ Frontend/src/pages/index.js
@@ -0,0 +1,17 @@
+import AddPost from "./AddPost";
+import AllPosts from "./AllPosts";
+import EditPost from "./EditPost";
+import Home from "./Home";
+import Login from "./Login";
+import Signup from "./Signup";
+import Post from "./Post";
+
+export {
+    AddPost,
+    EditPost,
+    AllPosts,
+    Home,
+    Login,
+    Signup,
+    Post,
+}
