6cd060b9f1a4d2cbbf9fd53ac1dd3bef88cee7e2
vaishnavPatange <patangevaishnav5@gmail.com>
2025-01-13 23:18:56 +0530

    add remaining post controllers

--- Backend/src/controllers/post.controller.js
+++ Backend/src/controllers/post.controller.js
@@ -3,32 +3,43 @@ const ApiErrors = require("../utils/ApiErrors.js");
 const ApiResponse = require("../utils/ApiResoponse.js");
 const cloudinaryUpload = require("../utils/cloudinary.js");
 const Post = require("../model/post.model.js");
+const mongoose = require("mongoose");
+const { isValidObjectId } = mongoose;
 
 const getAllPosts = asyncHandler( async( req, res) => {
-    const {page=1, limit=30, query, sortType, userId} = req.query;
+    const {page=1, limit=30, query, sortBy,sortType, userId} = req.query;
 
     const pageNo = parseInt(page);
     const pageLimit = parseInt(limit);
     const skip = (pageNo - 1) * pageLimit;
 
-    const filter = {};
+    const pipeline = [];
 
-    if(query.trim() !== ""){
-        filter.$or = [
-            {title: {$regex: query, $option: "i"}},
-            {slug: {$regex: query, $option: "i"}},
-            {content: {$regex: query, $option: "i"}}
+    const matchStage = {};
+    if( query && query.trim() !== ""){
+        matchStage.$or = [
+            {title : { $regex: query, $options: "i"}},
+            {slug : { $regex: query, $options: "i"}},
+            {content : { $regex: query, $options: "i"}},
         ]
-    };
-
-    if(userId){
-        filter.user = userId
     }
 
-    const posts = await Post.find(filter)
-    .sort({sortBy: sortType})
-    .limit(pageLimit)
-    .skip(skip);
+    if(userId) matchStage.user = userId;
+
+    if(Object.keys(matchStage) > 0) pipeline.push({$match: matchStage});
+
+    if( sortBy && sortType){
+        const sortStage = {
+            $sort: {[sortBy]: parseInt(sortType)}
+        }
+        pipeline.push(sortStage);
+    }
+
+    if(skip) pipeline.push({$skip: skip});
+
+    pipeline.push({$limit: pageLimit});
+
+    const posts = await Post.aggregate(pipeline);
 
     const totalPosts = await Post.countDocuments();
     const totalPages = Math.ceil(totalPosts/pageLimit);
@@ -101,4 +112,81 @@ const editPost = asyncHandler( async(req, res) => {
         "Post updated successfully"
     ));
 
-})
+});
+
+const deletePost = asyncHandler( async(req, res)=> {
+    const { _id } = req.params;
+
+    if(!_id) throw new ApiErrors(400, "Post id is missing");
+
+    if(!isValidObjectId(_id)) throw new ApiErrors(400, "Invalid post id");
+
+    const post = await Post.findOne({_id, user: req.user?._id});
+
+    if(!post) throw new ApiErrors(401, "Post not found or You are not owner of this post");
+
+    await Post.findByIdAndDelete(_id);
+
+    return  res.status(200)
+    .json( new ApiResponse(
+        200,
+        {},
+        "Post deleted successfully"
+    ));
+
+});
+
+const getPost = asyncHandler( async(req, res) => {
+    const {_id} = req.params;
+
+    if(!_id) throw new ApiErrors(400, "Post id missing");
+
+    if(!isValidObjectId(_id)) throw new ApiErrors(400, "Invalid post id");
+
+    const post = await Post.aggregate([
+        {
+            $match: {
+                _id: new mongoose.Types.ObjectId(_id) 
+            }
+        },
+        {
+            $lookup: {
+                from: "User",
+                localField: "user",
+                foreignField: "_id",
+                as: "user",
+                pipeline: [
+                    {
+                        $project: {
+                            username: 1,
+                            avatar: 1
+                        }
+                    }
+                ]
+            }
+        },
+        {
+            $addFields: {
+                user: {
+                    $first: "$user"
+                }
+            }
+        }
+    ]);
+
+    return res.status(200)
+    .json( new ApiResponse(
+        200,
+        post[0],
+        "Post fetched successfully"
+    ));
+
+});
+
+module.exports = {
+    getAllPosts,
+    addPost,
+    editPost,
+    deletePost,
+    getPost
+}
